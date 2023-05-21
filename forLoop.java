e72ebcbcc5e8f069bd25da69421a96b783344d55
Vikas Patkar <vikkipatkar08@gmail.com>
2023-05-21 23:11:51 +0530

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class forLoop {
+    public static void main(String[] args) {
+        // printing the even numbers using for loop
+        int i;
+
+        for ( i = 2; i<=100; i+= 2){
+                    System.out.println(i);
+                }
+        System.out.println("outside the loop i is " + i);
+    }
+}
