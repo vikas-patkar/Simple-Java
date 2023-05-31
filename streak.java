3a26eb17a9710c65ac289efcc3735ff2d58d9976
Vikas Patkar <vikkipatkar08@gmail.com>
2023-05-31 23:54:28 +0530

    Add files via upload

---
+++ streak.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class streak {
+    public static void main(String[] args) {
+        System.out.println("this program is just written to save the streak");
+        System.out.println("oh wait...i missed it by a minute ");
+    }
+}
